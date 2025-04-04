078bd9c51056159bb4b04e3f5a7aed1e3578f6bc
Vrglab <vrglab1@gmail.com>
2025-04-04 10:43:23 +0200

    WIP: Finished the Mitarbeiter and implemnted part of the Tests

--- src/main/java/org/Vrglab/Mitarbeiter.java
+++ src/main/java/org/Vrglab/Mitarbeiter.java
@@ -1,4 +1,16 @@
 package org.Vrglab;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Date;
+
+@Getter
+@AllArgsConstructor
 public class Mitarbeiter {
+    private String name;
+    private Date FZG;
+    private double performance;
+    private int abgesProjekte, fehlTage;
+    private boolean teamLeader;
 }
