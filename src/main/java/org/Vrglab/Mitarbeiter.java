10d14b6b453091da237b8bdf986bddc9eb2f5123
Vrglab <vrglab1@gmail.com>
2025-05-09 10:30:52 +0200

    WIP: Fixed tests and Inconsistent logic

--- src/main/java/org/Vrglab/Mitarbeiter.java
+++ src/main/java/org/Vrglab/Mitarbeiter.java
@@ -3,13 +3,14 @@ package org.Vrglab;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 @Getter
 @AllArgsConstructor
 public class Mitarbeiter {
     private String name;
-    private Date FZG;
+    private LocalDate FZG;
     private double performance;
     private int abgesProjekte, fehlTage;
     private boolean teamLeader;
